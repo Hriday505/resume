76a01f82e7104262209d5df3d4e95719a2dfdfc5
Hriday505 <121348381+Hriday505@users.noreply.github.com>
2023-06-27 04:22:13 +0530

    Add files via upload

--- MyScript.js
+++ MyScript.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-const sideMenu = document.querySelector("aside");
+/*const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const themeToggler = document.querySelector(".theme-toggler");
@@ -22,8 +22,45 @@ themeToggler.addEventListener('click',() => {
 	document.body.classList.toggle('dark-theme');
 	themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
 	themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
-});
+});*/
 
 
 // JavaScript Document
+
+
+let list = document.querySelectorAll(".nevigation li");
+
+
+function activelink(){
+	list.forEach(item=>{
+				  
+				  item.classList.remove("hovered");
+				  
+				  })
+				  
+			this.classList.add("hovered");	
+	      
+}
+
+
+list.forEach(item => item.addEventListener("mouseover",activelink));
+
+//================menu toggle================*/
+let toggler = document.querySelector(".toggle");
+let nav = document.querySelector(".nevigation");
+let main = document.querySelector(".main");
+
+ toggler.onclick = function(){
+
+	 nav.classList.toggle("active");
+	 main.classList.toggle("active");
+	 	 
+ }
+ 
+ 
+		
+
+
+
+
 
